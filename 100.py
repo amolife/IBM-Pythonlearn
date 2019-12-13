77a4614aef5ca3da5a0342279911e3e75b817035
amolife <amolife@163.com>
2019-12-13 11:28:49 +0800

    peep8

--- 100.py
+++ 100.py
@@ -7,5 +7,4 @@ a = n % 10
 b = n // 10 % 10
 c = n // 100 % 10
 
-print(a,b,c)
-
+print(a, b, c)
--- fibonacci2.py
+++ fibonacci2.py
@@ -2,9 +2,7 @@
 # _*_ coding: UTF-8 _*_
 
 print("output 50 fibs:")
-fibs = [1,1]
+fibs = [1, 1]
 for i in range(50):
     fibs.append(fibs[i]+fibs[i+1])
 print(fibs)
-
-
