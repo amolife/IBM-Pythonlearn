8a1fd194406a60cf536b230f9c8469a895d9898e
amolife <amolife@163.com>
2019-12-13 11:11:37 +0800

    ppep8

--- 17.py
+++ 17.py
@@ -19,4 +19,5 @@ for c in s:
         digit += 1
     else:
         others += 1
-print('char = %d,space = %d,digit = %d,others = %d' % (letters,space,digit,others))
+print('char = %d,space = %d,digit = %d,others = %d' %
+      (letters, space, digit, others))
